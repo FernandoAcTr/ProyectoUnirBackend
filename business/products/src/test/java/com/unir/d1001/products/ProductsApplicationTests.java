ef81d186185edf5a6e260c6384ce2c5e13721f68
Fernando Acosta <lacosta@gobdigital.com>
2025-02-25 14:47:36 -0600

    feat: setup de los microservicios principales de springboot

---
+++ business/products/src/test/java/com/unir/d1001/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.d1001.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
