fdf6835ddca8d07554c2b03c847b1966b5f88f3a
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-02-25 23:41:30 -0600

    fix: actualizar estrategia de generación de ID y agregar nombres de tablas para entidades Token y User

--- business/auth/src/main/java/com/unir/d1001/auth/entities/Token.java
+++ business/auth/src/main/java/com/unir/d1001/auth/entities/Token.java
@@ -7,15 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
 @Builder
+@Table(name = "tokens")
 public class Token {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public String token;
--- business/auth/src/main/java/com/unir/d1001/auth/entities/User.java
+++ business/auth/src/main/java/com/unir/d1001/auth/entities/User.java
@@ -8,16 +8,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
 @Builder
+@Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public String email;
