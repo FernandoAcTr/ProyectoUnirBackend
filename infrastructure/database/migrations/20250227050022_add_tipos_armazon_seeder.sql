f543de6988c382903dd64c95c9494e5c6acdf574
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-02-26 23:03:00 -0600

    feat: agregar seeders para formas, tipos de armazones y marcas en la base de datos

---
+++ infrastructure/database/migrations/20250227050022_add_tipos_armazon_seeder.sql
@@ -0,0 +1,6 @@
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('1', 'Ranurado');
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('2', 'Aro Completo');
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('3', 'Volado');
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('4', 'Acetato');
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('5', 'Metálico');
+INSERT INTO tipo_armazones (id, descripcion) VALUES ('6', 'Flex');
