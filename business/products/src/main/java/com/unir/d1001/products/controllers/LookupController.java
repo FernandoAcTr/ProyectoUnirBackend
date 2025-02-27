ec1a5bd2dc34a853f1fe8eb3b387039713fc5e54
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-02-26 23:21:58 -0600

    feat: configurar aplicación de productos con propiedades de base de datos y ajustar rutas de API

--- business/products/src/main/java/com/unir/d1001/products/controllers/LookupController.java
+++ business/products/src/main/java/com/unir/d1001/products/controllers/LookupController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/lookups")
+@RequestMapping("/lookups")
 @RequiredArgsConstructor
 public class LookupController {
 
