d6345917ab0ec47764a41db56ea980eef894eaa6
Fernando Acosta <lacosta@gobdigital.com>
2025-02-28 14:45:52 -0600

    feat: microservicio de api gateway

--- business/products/src/main/java/com/unir/d1001/products/controllers/LookupController.java
+++ business/products/src/main/java/com/unir/d1001/products/controllers/LookupController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lookups")
+@RequestMapping("/products/lookups")
 @RequiredArgsConstructor
 public class LookupController {
 
