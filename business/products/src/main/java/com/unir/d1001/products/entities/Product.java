3a7e79697af1359c61d944b561ce12778fef4085
Fernando Acosta <47435462+FernandoAcTr@users.noreply.github.com>
2025-03-01 17:28:26 -0600

    feat: agregar relación uno a muchos entre Product y ProductDetail (#12)

--- business/products/src/main/java/com/unir/d1001/products/entities/Product.java
+++ business/products/src/main/java/com/unir/d1001/products/entities/Product.java
@@ -1,6 +1,9 @@
 package com.unir.d1001.products.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,37 +26,40 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @Column(nullable = false)
     private Double precio;
-    
+
     @Column(nullable = false)
     private String descripcion;
-    
+
     @ManyToOne
     @JoinColumn(name = "tipo_armazon_id")
     private TipoArmazon tipoArmazon;
-    
+
     @ManyToOne
     @JoinColumn(name = "marca_id")
     private Marca marca;
-    
+
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Category categoria;
-    
+
     @ManyToOne
     @JoinColumn(name = "forma_id")
     private Forma forma;
-    
+
+    @OneToMany(mappedBy = "producto")
+    @JsonManagedReference
+    private List<ProductDetail> details;
+
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-    
+
     @Column(nullable = false)
     private LocalDateTime updatedAt;
-
 }
