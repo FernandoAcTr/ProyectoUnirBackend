c747ee9b831a830e08eeae0e591be47f08a055a3
Fernando Acosta <47435462+FernandoAcTr@users.noreply.github.com>
2025-03-01 17:19:35 -0600

    Fix/cors (#11)
    * feat: agregar configuración de CORS en los servicios de pedidos y productos
    
    * feat: agregar configuración global de CORS en el servidor API Gateway

---
+++ business/orders/src/main/java/com/unir/d1001/orders/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.unir.d1001.orders.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cc = new CorsConfiguration();
+        cc.setAllowedHeaders(java.util.List.of("*"));
+        cc.setExposedHeaders(java.util.List.of("Content-Disposition"));
+        cc.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        cc.setAllowedOrigins(java.util.List.of("*"));
+        cc.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cc);
+
+        return source;
+    }
+}
