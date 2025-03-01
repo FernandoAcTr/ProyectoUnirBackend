7de8b6be42b166ddf27c43f4b128ac1dcaf3ab14
Fernando Acosta <fernandodeveloper99@gmail.com>
2025-03-01 17:28:12 -0600

    feat: agregar relación uno a muchos entre Product y ProductDetail

--- business/products/src/main/java/com/unir/d1001/products/entities/ProductDetail.java
+++ business/products/src/main/java/com/unir/d1001/products/entities/ProductDetail.java
@@ -1,5 +1,7 @@
 package com.unir.d1001.products.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,34 +22,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDetail {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @ManyToOne
     @JoinColumn(name = "producto_id")
+    @JsonBackReference
     private Product producto;
-    
+
     @Column(nullable = false)
     private String color;
-    
+
     @Column(nullable = false)
     private String talla;
-    
+
     @Column(nullable = false)
     private String longitudVarilla;
-    
+
     @Column(nullable = false)
     private String anchoPuente;
-    
+
     @Column(nullable = false)
     private String anchoTotal;
-    
+
     @ManyToOne
     @JoinColumn(name = "image_id")
     private Image image;
-    
+
     @Column(nullable = false)
     private String sku;
 
